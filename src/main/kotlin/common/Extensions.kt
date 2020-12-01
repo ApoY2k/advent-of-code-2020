e9c13a262c4db8a00e31c616212fc99dcda0eeb1
ApoY2k <f.p@mailbox.org>
2020-12-01 08:32:55 +0100

    Day 01 solution

--- src/main/kotlin/common/Extensions.kt
+++ src/main/kotlin/common/Extensions.kt
@@ -1,2 +1,13 @@
 package common
 
+import java.io.File
+
+val dummy = {}.javaClass
+
+fun readInputTextFile(path: String): List<String> {
+    return File(dummy.getResource(path).toURI()).readLines()
+}
+
+fun readInputTextFileInt(path: String): List<Int> {
+    return readInputTextFile(path).map { it.toInt() }
+}
--- src/main/kotlin/day01/Main.kt
+++ src/main/kotlin/day01/Main.kt
@@ -1,2 +1,23 @@
 package day01
 
+import common.readInputTextFileInt
+
+fun main() {
+    val input = readInputTextFileInt("/day01/input.txt")
+    var result: Int? = null
+    input.forEach { first ->
+        val firstIndex = input.indexOf(first)
+        val firstRest = input.subList(firstIndex, input.lastIndex)
+        firstRest.forEach { second ->
+            val secondIndex = input.indexOf(second)
+            val secondRest = input.subList(secondIndex, input.lastIndex)
+            secondRest.forEach { third ->
+                if (first + second + third == 2020) {
+                    result = first * second * third
+                    return@forEach
+                }
+            }
+        }
+    }
+    println("Result: $result")
+}
